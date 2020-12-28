af8e2a8381fe9d712b5bd5365a7851eb5c4f66b1
Rajeswari Arumugam <arumugam.ra@pg.com>
2020-12-28 14:04:40 +0530

    Adding Function app

--- Function1.cs
+++ Function1.cs
@@ -1,30 +1,60 @@
-
+using System;
 using System.IO;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net;
+using System.Xml;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.ApplicationInsights;
 
-namespace IntelligentPackageRoutingFunctionApp
+namespace FunctionApp
 {
     public static class Function1
     {
-        [FunctionName("Function1")]
-        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
-        {
-            log.Info("C# HTTP trigger function processed a request.");
+		[FunctionName("Function1")]
+		public static HttpResponseMessage Run(
+			[HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "01/{gtin}")] HttpRequest req,
+			ILogger log, string gtin, ExecutionContext context)
+		{
+			string requestUrl = Microsoft.AspNetCore.Http.Extensions.UriHelper.GetDisplayUrl(req);
 
-            string name = req.Query["name"];
+			log.LogInformation("Request Url", requestUrl);
 
-            string requestBody = new StreamReader(req.Body).ReadToEnd();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+			string OriginUrl = string.Empty;
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
-        }
-    }
+			var data = File.ReadAllText(context.FunctionAppDirectory + "/GtinUrl.xml");
+
+			XmlDocument doc = new XmlDocument();
+			doc.LoadXml(data);
+			foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+			{
+				if (node.ChildNodes[0]?.InnerText == gtin)
+				{
+					OriginUrl = node.ChildNodes[1].InnerText;
+				}
+			}
+
+			log.LogInformation("Redirect Url", OriginUrl);
+
+			HttpRequestMessage request = new HttpRequestMessage();
+			var response = request.CreateResponse(HttpStatusCode.TemporaryRedirect);
+
+			if (OriginUrl != null && OriginUrl != "")
+			{
+				response.Headers.Location = new Uri(OriginUrl);
+			}
+			else
+			{
+				return request.CreateResponse(HttpStatusCode.InternalServerError);
+			}
+
+			return response;
+		}
+	}
 }
